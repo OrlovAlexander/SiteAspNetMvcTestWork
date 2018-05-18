5988fa49e65c13f91920253e4a5cc6d69934c3fb
OrlovAlexander <oaln@yandex.ru>
2018-05-18 14:09:29 +0300

    Исправлен информационный скрипт

--- ElmaTestWork_2/Scripts/SiteInformationTimer.js
+++ ElmaTestWork_2/Scripts/SiteInformationTimer.js
@@ -2,30 +2,35 @@
 //~/Scripts/SiteInformationTimer.js
 
 $(document).ready(function () {
-    if (document.getElementById("infoSection") === undefined) {
+    if (document.getElementById("infoSection") === undefined
+        || document.getElementById("infoSection") === null) {
         return;
     }
-    var informationStore = function () {
-        newUrl = document.location.protocol + "//" + document.location.host + "/Document/InformationStore";
-        $.ajax({
-            url: newUrl,
-            type: 'POST',
-            data: {}
-        }).done(function (data) {
-            if (data.Result === "OK") {
-                if (document.getElementById("fileCount") !== undefined) {
-                    document.getElementById("fileCount").innerText = data.Count;
-                }
-                if (document.getElementById("fileVolume") !== undefined) {
-                    document.getElementById("fileVolume").innerText = data.Volume;
-                }
-            }
-            else if (data.Result !== undefined) {
-                console.error(data.Result.Message);
-            }
-        }).fail(function () {
-            console.error("There is something wrong. Please try again.");
-        });
-        setTimeout(informationStore, 1000);
-    }
+    setTimeout(informationStore, 1000);
 });
+
+var informationStore = function () {
+    newUrl = document.location.protocol + "//" + document.location.host + "/Document/InformationStore";
+    $.ajax({
+        url: newUrl,
+        type: 'POST',
+        data: {}
+    }).done(function (data) {
+        if (data.Result === "OK") {
+            if (document.getElementById("fileCount") !== undefined
+                && document.getElementById("fileCount") !== null) {
+                document.getElementById("fileCount").innerText = data.Count;
+            }
+            if (document.getElementById("fileVolume") !== undefined
+                && document.getElementById("fileVolume") !== null) {
+                document.getElementById("fileVolume").innerText = data.Volume;
+            }
+        }
+        else if (data.Result !== undefined) {
+            console.error(data.Result.Message);
+        }
+    }).fail(function () {
+        console.error("There is something wrong. Please try again.");
+    });
+    setTimeout(informationStore, 1000);
+}
